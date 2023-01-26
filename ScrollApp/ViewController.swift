cc8a4f694573ce3b0b4c1c17dfb6db75c1ba241c
vbugrym <vbugrym@lohika.com>
2023-01-26 14:49:07 +0200

    Set basic ui

--- ScrollApp/ViewController.swift
+++ ScrollApp/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var collection: UICollectionView!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        
+        collection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        setupUI()
     }
-
-
+    
+    func setupUI() {
+        guard let collection = collection else { return }
+        self.view.addSubview(collection)
+        collection.backgroundColor = .red
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: collection, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 200).isActive = true
+        NSLayoutConstraint(item: collection, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: collection, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: collection, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
+    }
 }
 
