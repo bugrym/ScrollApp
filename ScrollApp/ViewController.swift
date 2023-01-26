7806e0eb4493dc747f6c6d71a9b4a2ea3a0c54ce
vbugrym <vbugrym@lohika.com>
2023-01-26 15:23:17 +0200

    Dynamic item population

--- ScrollApp/ViewController.swift
+++ ScrollApp/ViewController.swift
@@ -18,7 +18,22 @@ class Cell: UICollectionViewCell {
 class ViewController: UIViewController {
 
     private var collection: UICollectionView!
-  
+    private var dataSource: Int = 0 {
+        didSet {
+            collection.reloadData()
+            let indexPath = IndexPath(item: dataSource - 1, section: 0)
+            collection.scrollToItem(at: indexPath, at: .right, animated: true)
+        }
+    }
+    
+    private lazy var addBtn: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Add Item", for: .normal)
+        btn.backgroundColor = .blue
+        btn.addTarget(self, action: #selector(addBtnTap), for: .touchUpInside)
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,23 +47,37 @@ class ViewController: UIViewController {
     
     func setupUI() {
         guard let collection = collection else { return }
+        self.view.addSubview(addBtn)
         self.view.addSubview(collection)
+        
         collection.dataSource = self
-        collection.delegate = self 
+        collection.delegate = self
         collection.backgroundColor = .red
         collection.register(Cell.self, forCellWithReuseIdentifier: Cell.reuseID)
+        addBtn.translatesAutoresizingMaskIntoConstraints = false
         collection.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: addBtn, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 56).isActive = true
+        NSLayoutConstraint(item: addBtn, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1, constant: 56 * 2.5).isActive = true
+        NSLayoutConstraint(item: addBtn, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: addBtn, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 60).isActive = true
+        
         
         NSLayoutConstraint(item: collection, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 200).isActive = true
         NSLayoutConstraint(item: collection, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 0).isActive = true
         NSLayoutConstraint(item: collection, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: 0).isActive = true
         NSLayoutConstraint(item: collection, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
     }
+    
+    @objc private
+    func addBtnTap() {
+        dataSource += 1
+    }
 }
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return dataSource
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
